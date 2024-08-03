dc7e02a2307f944b30e0bb5c318d480ace3ebe69
sebi <gore.sebyx@yahoo.com>
2024-08-03 16:29:18 +0100

    clean

--- Gemfile
+++ Gemfile
@@ -12,3 +12,5 @@ gem 'rspec', '~> 3.0'
 gem 'rubocop-rails_config', '~> 1.16'
 
 gem 'lefthook', '~> 1.7', '>= 1.7.4'
+
+gem 'pry', '~> 0.14.2'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'binary_search'
+require 'pry'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
